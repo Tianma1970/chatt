754d6ca0e64fd733c0241f71dc870a91f1cbd647
Tillmann Weimer <tillmann1970@gmail.com>
2020-10-30 10:20:09 +0100

    wrapped into anonymos function

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-//Get peer id (hash) from URL
-const peerId = location.hash.slice(1)
+;(function () {
+  //anonym function
+  //Get peer id (hash) from URL
+  const peerId = location.hash.slice(1)
 
-//Connect to peer server
+  //Connect to peer server
 
-peer = new Peer(peerId, {
-  host: "glajan.com",
-  port: 8443,
-  path: "/myapp",
-  secure: true
-})
-
-peer.on("open", id => {
-  const peerIdEl = document.querySelector(".my-peer-id")
-
-  peerIdEl.innerText = id
-})
-peer.on("error", errorMessage => {
-  console.error(errorMessage)
-})
-
-// Event listener for click "Refresh list"
-const refreshPeersListButtonEl = document.querySelector(".list-all-peers-button")
-refreshPeersListButtonEl.addEventListener("click", () => {
-  peer.listAllPeers(peers => {
-    const peerListEl = document.querySelector(".peers")
-    const peerEl = peers
-      .filter(peerId => {
-        if (peerId === peer._id) return false
-        return true
-      }) //remove our own name
-      .map(peer => {
-        return `<li><button>${peer}</button></li>`
-      })
-      .join("")
-
-    const ulEl = document.createElement("ul")
-    ulEl.innerHTML = peerEl
-    peerListEl.appendChild(ulEl)
+  peer = new Peer(peerId, {
+    host: "glajan.com",
+    port: 8443,
+    path: "/myapp",
+    secure: true
   })
-  console.log("click")
-})
+
+  peer.on("open", id => {
+    const peerIdEl = document.querySelector(".my-peer-id")
+
+    peerIdEl.innerText = id
+  })
+  peer.on("error", errorMessage => {
+    console.error(errorMessage)
+  })
+
+  // Event listener for click "Refresh list"
+  const refreshPeersListButtonEl = document.querySelector(".list-all-peers-button")
+  refreshPeersListButtonEl.addEventListener("click", () => {
+    peer.listAllPeers(peers => {
+      const peerListEl = document.querySelector(".peers")
+      const peerEl = peers
+        .filter(peerId => {
+          if (peerId === peer._id) return false
+          return true
+        }) //remove our own name
+        .map(peer => {
+          return `<li><button>${peer}</button></li>`
+        })
+        .join("")
+
+      const ulEl = document.createElement("ul")
+      ulEl.innerHTML = peerEl
+      peerListEl.appendChild(ulEl)
+    })
+    console.log("click")
+  })
+})() //end of anonym function. you need to add '()'
